5cc9ce017d4385af233e142407b73141b829eed3
Henrique Breim <henrique@breim.com.br>
2015-07-19 00:33:06 -0300

    Add paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,4 +34,26 @@ class User < ActiveRecord::Base
   	update_attributes(params) 
   end
   # --
+
+  # PaperClip
+  has_attached_file :image,:styles => {
+    :large => "512x512" ,
+    :medium => "200x200" ,
+    :small => "168x168",
+    :thumb => "40x40",
+    :tiny => "32x32"
+    },
+    :url  => "/images/users/:id-:basename.:style.:extension",
+    :path => ":rails_root/public/images/users/:id-:basename.:style.:extension",
+    :default_url => :set_default_url
+
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    validates_attachment_size :image, :less_than => 10.megabytes
+
+  # if user dont have image
+  def set_default_url
+    "/img/missing.png"
+  end
+  # End ---
+
 end
